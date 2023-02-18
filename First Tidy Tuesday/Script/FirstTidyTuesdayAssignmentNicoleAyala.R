550044023aedd14469cfcf6c8b9f72bcce96e1b4
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-17 20:45:40 -0800

    was too ambitious and I still need to practice so simple scatterplot shall do for now, this is the script for it

--- First Tidy Tuesday/Script/FirstTidyTuesdayAssignmentNicoleAyala.R
+++ First Tidy Tuesday/Script/FirstTidyTuesdayAssignmentNicoleAyala.R
@@ -7,29 +7,24 @@
 library(tidyverse)
 library(here)
 library(devtools)
-library(ggrforce)
-l
+library(ggridges)
+library(beyonce)
 library(ggplot2)
 
 ### Loading in Data #####
 movies<- read_csv(here("FirstTidyTuesday","Data","movies.csv.com"))
 View(movies)
 glimpse(movies)
-movies_clean<-movies %>% # Always remember the pipe! :)
-  filter(complete.cases(.)) #filters out everything that is not a complete row
-  filter(.data = movies_clean, age < 50, gender == "woman")
 
-movies %>%
-  ggplot(aes(x = `Release Year`,
-             y = `Actor 2 Age`)) +
-  ggforce::geom_sina(
-    maxwidth = .6, scale = "count", seed = 1,
-    size = 7, alpha = .5
-  ) + 
-  ggforce::geom_sina(
-    maxwidth = .6, scale = "count", seed = 1, 
-    size = 7, shape = 1, color = "blue",
-    stroke = .8
-  )
- 
+
+
+movieplot<-movies %>%
+  ggplot(aes(x = `Release Year`, # my x label
+             y = `Age Difference`)) + # my y label
+  geom_point(size=0.5, color = "maroon") +
+  labs(x = "Age Difference",y = "Release Year", Title = "Hollywood Directors Age Differences over the Years")
+  
+ggsave(here("FirstTidyTuesday","Output","FirstTidyTuesdayAssignmentNicoleAyala.png"))
+
+movieplot
  
