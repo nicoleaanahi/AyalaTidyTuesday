733a565b7d3b3decd9db84bcb8c7c05ca16cb1f0
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-17 20:48:32 -0800

    made changes to the name of my png file within my script

--- First Tidy Tuesday/Script/FirstTidyTuesdayAssignmentNicoleAyala.R
+++ First Tidy Tuesday/Script/FirstTidyTuesdayAssignmentNicoleAyala.R
@@ -16,15 +16,17 @@ movies<- read_csv(here("FirstTidyTuesday","Data","movies.csv.com"))
 View(movies)
 glimpse(movies)
 
-
+# thought I would start simple with my first tidy tuesday and I'll be more creative in my next one
+# PIPES ARE IMPORTANT
 
 movieplot<-movies %>%
   ggplot(aes(x = `Release Year`, # my x label
              y = `Age Difference`)) + # my y label
   geom_point(size=0.5, color = "maroon") +
-  labs(x = "Age Difference",y = "Release Year", Title = "Hollywood Directors Age Differences over the Years")
-  
-ggsave(here("FirstTidyTuesday","Output","FirstTidyTuesdayAssignmentNicoleAyala.png"))
+  labs(x = "Age Difference",y = "Release Year", Title = "Hollywood Directors Age Differences over the Years") # CONFUSED AS TO WHY MY TITLE IS NOT PRESENT, BUT SMALL SIZE MAKES DATA SEEM LESS MERGED TOGETHER
+
+
+
+ggsave(here("FirstTidyTuesday","Output","FirstTidyTuesdayAssignment(2023-02-17).png"))
 
 movieplot
- 
