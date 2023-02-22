d2385aff68a1a1231c539594ec9c6d1f9b0830cb
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-21 23:33:00 -0800

    WIP regarding script for tidy tuesday #2

--- Second Tidy Tuesday/Script/Second Tidy Tuesday Assignment.R
+++ Second Tidy Tuesday/Script/Second Tidy Tuesday Assignment.R
@@ -7,21 +7,22 @@ library(tidyverse)
 library(tidytuesdayR)
 library(here)
 library(readr)
+library(devtools)
+library(ggdist)
+library(ggplot2)
+library(BobRossColors)
+library(ggridges)
+library(ggdist)
+
+
 
 
 ###### Loading Data #####
-urlfile="https://raw.githubusercontent.com/jwilber/Bob_Ross_Paintings/master/data/bob_ross_paintings.csv"
+bobross_data<- read_csv(here("Second Tidy Tuesday","Data", "bob_ross.csv"))
+glimpse(bob_ross) 
+View(bob_ross)
 
-mydata<-read_csv(url(urlfile))
 
 
-glimpse(bob_ross) # wanna see how the data looks prior to choosing the data I'd liek to plot
-
-bob_ross <- select(bob_ross, -1) + # removing the first column that does not contain any necessary data
-
-bob_ross <- bob_ross |> 
-  mutate(across(Black_Gesso:Alizarin_Crimson, as.logical))
-# saving the data
-  bob_ross<- read_csv(here("Second Tidy Tuesday", "Data", "bob_ross.csv"))
 
     
