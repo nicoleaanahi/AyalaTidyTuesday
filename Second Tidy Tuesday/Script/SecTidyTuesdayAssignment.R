041b1406b2328ce842ff1a69c915c27c153aab00
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-02-25 19:25:51 -0800

    updated script

--- Second Tidy Tuesday/Script/SecTidyTuesdayAssignment.R
+++ Second Tidy Tuesday/Script/SecTidyTuesdayAssignment.R
@@ -17,8 +17,8 @@ library(ggraph)
 
 
 ###### Loading Data #####
-bobross_data<- read_csv(here("Second Tidy Tuesday","Data", "bob_ross.csv")) # time to laod in the data :)
-glimpse(bobross_data) 
+bobross<- read_csv(here("Second Tidy Tuesday","Data", "bob_ross.csv")) # time to laod in the data :)
+glimpse(bobross) 
 View(bobross_data) # see if data works
 select(-contains("src")) %>% # pipe itself so it is easily readable and replicable
   filter(season <= 31) %>% # filter out seasons through 31 and under
@@ -27,7 +27,7 @@ select(-contains("src")) %>% # pipe itself so it is easily readable and replicab
 
 # time to plot :D
 
-bobross_data %>%
+bobross_data<-bobross %>%
   pivot_longer(cols = 8:25, names_to = "color") %>% # the cols you want to pivot. This says select the color cols form 8 to 25 and change it to colors
   filter(value) %>% #filter out all the values that do not correlate to color
   mutate(color = str_replace_all(color, "_", " ")) %>% # want to replace all the different variations of color names into a cohesive manner
@@ -35,11 +35,11 @@ bobross_data %>%
   slice_max(n, n = 10, with_ties = FALSE) %>% #select rows with highest or lowest values of a variable, n =population
   
   ggplot(aes(n, color, fill = season)) + #fill each season with 10 colors
-  facet_wrap(~season, scales ="free") #releases both x and y axis
+  facet_wrap(~season, scales ="free") + #releases both x and y axis
   labs(x = "season", # my x label
        y = "color", # my y label
-       title = "The 10 Most Common Colors Per Season") + # title of my plot
-    plot.background = element_rect(fill = "yellow")  # not sure as to why it is not working
+       title = "The 10 Most Common Colors Per Season") # title of my plot
+  
 
 ggsave(here("Second Tidy Tuesday","Output","SecondTidyTuesdayAssignment(2023-02-21).png"))
 
