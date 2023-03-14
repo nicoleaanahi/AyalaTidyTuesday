4519f44a3e6a6bd705faad72b4068b0efbbdbe7e
Nicole Ayala <nicoleanahiayala01@gmail.com>
2023-03-14 15:06:30 -0700

    in progress

---
+++ Fifth Tidy Tuesday/Script/Fifth Tidy Tuesday Script.R
@@ -0,0 +1,35 @@
+
+### Fifth Tidy Tuesday Assignment!
+### Created by: Nicole Ayala
+### Created on: 2023-03-09
+### Updated on: 2023-03-10
+#####################################################################
+
+### Load Libraries ###
+library(tidyverse)
+library(here)
+library(tidytuesdayR)
+library(plyr)
+library(ggpubr)
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
+
+### Load the Data ###
+tuesdata <- tidytuesdayR::tt_load('2023-01-31')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
